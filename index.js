eb344c206bc54cc24bec2816b9c038ee3f6b1a7b
alikiminza <ali.kiminza@student.moringaschool.com>
2023-06-19 22:14:48 +0300

    final edit

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+let employee ={
+  name : 'Ali',
+  streetAddress : 'Mombasa'
+  }
+  
+  
+  
+  function updateEmployeeWithKeyAndValue(object, key, value){
+     return{
+  ...object,[key]:value,
+     };
+  }
+  
+  const updateObject = updateEmployeeWithKeyAndValue(employee, 'Moses', "Kisumu")
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value){
+       object[key] = value
+         return object;
+      }
+  
+  const destructiveObject = destructivelyUpdateEmployeeWithKeyAndValue(employee[streetAdress] = "Nyeri")
+  
+  function deleteFromEmployeeByKey(employee, key){
+    const old = Object.assign({},employee);
+    delete old[key];
+       return old;
+    }
+ 
+    function destructivelyDeleteFromEmployeeByKey(employee, key){
+     delete employee['name'];
+        return employee;
+     }
+  
